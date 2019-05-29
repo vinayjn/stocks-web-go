314acee3f39aea709cb070b5ecd137b733bf06e1
Vinay Jain <vinay.jn7@gmail.com>
2019-05-29 21:34:43 +0530

    Added stock search handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,9 +1,14 @@
 package handlers
 
-import (
-	"net/http"
-	"stocks/utils"
+import (	
+	"fmt"
 	"strings"
+	"net/http"
+	"io/ioutil"		
+	"stocks/utils"	
+	"stocks/models"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/jsonpb"
 )
 
 // Index says hello
@@ -14,5 +19,11 @@ func Index(w http.ResponseWriter, r *http.Request) {
 // SearchStocks searches for stocks which matches the query string
 func SearchStocks(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	utils.MakeGETCall("SYMBOL_SEARCH", map[string]string{"keywords": strings.Join(r.Form["keyword"], "")})
+	response, _ := utils.MakeGETCall("SYMBOL_SEARCH", map[string]string{"keywords": strings.Join(r.Form["keyword"], "")})
+	defer response.Body.Close()	
+	resp_body, _ := ioutil.ReadAll(response.Body)	
+	obj := models.SymbolSearchResponse{}	
+	jsonpb.Unmarshal(strings.NewReader(string(resp_body)), &obj)
+	data, _ := proto.Marshal(&obj)
+	k, _ := w.Write(data)	
 }
